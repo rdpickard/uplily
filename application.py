4dab889bacf8a2fb4bf9355e3b46d38344061439
Robert Daniel Pickard <robert.daniel.pickard@chalkfarm.org>
2020-01-09 23:31:48 -0500

    Trying to fix unsafe global var

--- application.py
+++ application.py
@@ -11,21 +11,22 @@ from werkzeug.utils import secure_filename
 
 application = flask.Flask(__name__)
 
-
-#TODO Need to change this static value of "/tmp/" as the dir to write uploaded files to
-trys = 0
-while trys < 10:
-    uploads_dir = "/tmp/{}/".format(''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]))
-    if os.path.exists(uploads_dir):
-        uploads_dir = None
-        trys += 1
-        continue
-    else:
-        os.mkdir(uploads_dir)
-        break
-if uploads_dir is None:
-    application.logger.error("Could not create temporary upload location on file system. Bailing")
-    sys.exit(-1)
+if "uploads_dir" not in application.config.keys():
+    #TODO Need to change this static value of "/tmp/" as the dir to write uploaded files to
+    trys = 0
+    while trys < 10:
+        uploads_dir = "/tmp/{}/".format(''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]))
+        if os.path.exists(uploads_dir):
+            uploads_dir = None
+            trys += 1
+            continue
+        else:
+            os.mkdir(uploads_dir)
+            break
+    if uploads_dir is None:
+        application.logger.error("Could not create temporary upload location on file system. Bailing")
+        sys.exit(-1)
+    application.config["uploads_dir"] = uploads_dir
 
 
 # P Gently stolen from https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
@@ -49,8 +50,8 @@ def upload_file():
 
     filename = secure_filename(file.filename)
 
-    application.logger.debug(os.path.join(uploads_dir, filename))
-    file.save(os.path.join(uploads_dir, filename))
+    application.logger.debug(os.path.join(application.config["uploads_dir"], filename))
+    file.save(os.path.join(application.config["uploads_dir"], filename))
 
     if flask.request.args.get("browser_upload", False):
         return flask.redirect("/", code=302)
@@ -66,11 +67,11 @@ def index():
     """
     uploaded_files = dict()
 
-    for filename in [f for f in os.listdir(uploads_dir) if os.path.isfile(uploads_dir+"/"+f)]:
+    for filename in [f for f in os.listdir(uploads_dir) if os.path.isfile(application.config["uploads_dir"]+"/"+f)]:
         uploaded_files[filename] = {"download_url": "{}dl/{}".format(flask.request.url_root, filename),
                                     "locale": "Local FS",
-                                    "md5_hash": md5_a_file(os.path.join(uploads_dir, filename)),
-                                    "file_size_in_bytes": os.stat(os.path.join(uploads_dir, filename)).st_size}
+                                    "md5_hash": md5_a_file(os.path.join(application.config["uploads_dir"], filename)),
+                                    "file_size_in_bytes": os.stat(os.path.join(application.config["uploads_dir"], filename)).st_size}
 
     return flask.render_template('index.jinja2',
                                  my_server=flask.request.url_root, uploaded_files_list=uploaded_files)
@@ -79,10 +80,10 @@ def index():
 @application.route('/dl/<string:filename>')
 def download_file(filename):
 
-    if filename not in os.listdir(uploads_dir):
+    if filename not in os.listdir(application.config["uploads_dir"]):
         flask.abort(404)
     else:
-        return flask.send_from_directory(uploads_dir, filename, as_attachment=True)
+        return flask.send_from_directory(application.config["uploads_dir"], filename, as_attachment=True)
 
 
 @application.before_first_request
